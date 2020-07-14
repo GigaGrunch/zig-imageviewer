81f6f2b684026c83e7c9a8f1502c351323b152b7
GigaGrunch <gigagrunch@gmail.com>
2020-07-14 17:45:46 +0200

    correctly handle 'close window' event

--- build.zig
+++ build.zig
@@ -3,6 +3,8 @@ const Builder = @import("std").build.Builder;
 pub fn build(builder: *Builder) void
 {
     const exe = builder.addExecutable("imageviewer", "code/main.zig");
+    exe.linkSystemLibrary("SDL2");
+    exe.linkSystemLibrary("c");
     builder.installArtifact(exe);
 
     const run_command = exe.run();
--- code/main.zig
+++ code/main.zig
@@ -1,6 +1,43 @@
 const debug = @import("std").debug;
+const c = @cImport(@cInclude("SDL2/SDL.h"));
 
-pub fn main() void
+pub fn main() !void
 {
-    debug.warn("Hello {}!\n", .{"World"});
+    c.SDL_Log("Hello World!");
+    defer c.SDL_Log("Bye bye!");
+
+    const window = c.SDL_CreateWindow
+    (
+        "Image Viewer",
+        c.SDL_WINDOWPOS_UNDEFINED,
+        c.SDL_WINDOWPOS_UNDEFINED,
+        1024, 768,
+        c.SDL_WINDOW_RESIZABLE
+    ) orelse return error.SDL_CreateWindow;
+
+    var app_running = true;
+    while (app_running)
+    {
+        var event: c.SDL_Event = undefined;
+        if (c.SDL_WaitEvent(&event) == 0)
+        {
+            return error.SDL_WaitEvent;
+        }
+
+        if (event.type == c.SDL_WINDOWEVENT)
+        {
+            const window_event = event.window;
+            if (window_event.event == c.SDL_WINDOWEVENT_CLOSE)
+            {
+                c.SDL_Log("Quitting");
+                app_running = false;
+            }
+        }
+    }
 }
+
+const SDLError = error
+{
+    SDL_CreateWindow,
+    SDL_WaitEvent
+};
