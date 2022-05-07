1695065003f5088b3bae7d895532c0c85b8c3fe9
GigaGrunch <gigagrunch@gmail.com>
2022-05-07 14:39:49 +0200

    simplify

--- code/main.zig
+++ code/main.zig
@@ -1,83 +1,69 @@
 const debug = @import("std").debug;
-const c = @cImport(@cInclude("SDL.h"));
+const sdl = @cImport(@cInclude("SDL.h"));
 
-pub fn main() !void
-{
-    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0)
-    {
+pub fn main() !void {
+    if (sdl.SDL_Init(sdl.SDL_INIT_VIDEO) != 0) {
         return error.SDL_Init;
     }
-    defer c.SDL_Quit();
+    defer sdl.SDL_Quit();
 
-    c.SDL_Log("Hello World!");
-    defer c.SDL_Log("Bye bye!");
+    sdl.SDL_Log("Hello World!");
+    defer sdl.SDL_Log("Bye bye!");
 
-    const window = c.SDL_CreateWindow
-    (
+    const window = sdl.SDL_CreateWindow(
         "Image Viewer",
-        c.SDL_WINDOWPOS_UNDEFINED,
-        c.SDL_WINDOWPOS_UNDEFINED,
+        sdl.SDL_WINDOWPOS_UNDEFINED,
+        sdl.SDL_WINDOWPOS_UNDEFINED,
         1024, 768,
-        c.SDL_WINDOW_RESIZABLE
+        sdl.SDL_WINDOW_RESIZABLE
     ) orelse return error.SDL_CreateWindow;
-    defer c.SDL_DestroyWindow(window);
+    defer sdl.SDL_DestroyWindow(window);
 
-    const renderer = c.SDL_CreateRenderer
-    (
+    const renderer = sdl.SDL_CreateRenderer(
         window, -1, 0
     ) orelse return error.SDL_CreateRenderer;
-    defer c.SDL_DestroyRenderer(renderer);
+    defer sdl.SDL_DestroyRenderer(renderer);
 
     const bytesPerPixel = 4;
     const width = 1024;
     const height = 768;
 
-    const texture = c.SDL_CreateTexture
-    (
+    const texture = sdl.SDL_CreateTexture(
         renderer,
-        c.SDL_PIXELFORMAT_RGBA8888,
-        c.SDL_TEXTUREACCESS_STATIC,
+        sdl.SDL_PIXELFORMAT_RGBA8888,
+        sdl.SDL_TEXTUREACCESS_STATIC,
         width, height
     ) orelse return error.SDL_CreateTexture;
-    defer c.SDL_DestroyTexture(texture);
+    defer sdl.SDL_DestroyTexture(texture);
 
     var memory = [_]u32{0} ** (width * height);
 
-    if (c.SDL_UpdateTexture
-    (
+    if (sdl.SDL_UpdateTexture(
         texture,
         null,
         &memory,
         width * bytesPerPixel
-    ) != 0)
-    {
-        return error.SDL_UpdateTexture;
-    }
+    ) != 0) return error.SDL_UpdateTexture;
 
     var app_running = true;
-    while (app_running)
-    {
-        var event: c.SDL_Event = undefined;
-        if (c.SDL_WaitEvent(&event) == 0)
-        {
+    while (app_running) {
+        var event: sdl.SDL_Event = undefined;
+        if (sdl.SDL_WaitEvent(&event) == 0) {
             return error.SDL_WaitEvent;
         }
 
-        if (event.type == c.SDL_WINDOWEVENT)
-        {
+        if (event.type == sdl.SDL_WINDOWEVENT) {
             const window_event = event.window;
-            if (window_event.event == c.SDL_WINDOWEVENT_CLOSE)
-            {
-                c.SDL_Log("Window event: SDL_WINDOWEVENT_CLOSE");
+            if (window_event.event == sdl.SDL_WINDOWEVENT_CLOSE) {
+                sdl.SDL_Log("Window event: SDL_WINDOWEVENT_CLOSE");
                 app_running = false;
             }
         }
 
-        if (c.SDL_RenderCopy(renderer, texture, null, null) != 0)
-        {
+        if (sdl.SDL_RenderCopy(renderer, texture, null, null) != 0) {
             return error.SDL_RenderCopy;
         }
 
-        c.SDL_RenderPresent(renderer);
+        sdl.SDL_RenderPresent(renderer);
     }
 }
